2fc499dda6883e9c6a3590ec915b1fc108095950
Rich Garrick <richg341@gmail.com>
2017-09-17 15:37:23 -0700

    add initial files

---
+++ app/typescript-addition.js
@@ -0,0 +1,7 @@
+var findSum = function (firstValue, secondValue) {
+    var sum = firstValue + secondValue;
+    alert("The sum of your two numbers is: " + sum);
+};
+var firstNumber = parseInt(prompt('please enter a number.'));
+var secondNumber = parseInt(prompt('enter another number.'));
+findSum(firstNumber, secondNumber);
